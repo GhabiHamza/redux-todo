be88847d0133d0da1fc4f35efec5fbcb2498e43f
GhabiHamza <ghabihamza77@gmail.com>
2019-09-10 17:25:25 +0200

    Updates

---
+++ precache-manifest.a531d35a00fec7f5979355efc6c09b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b411f7f807ad2f468dc915eac53c6d2a",
+    "url": "/redux-todo/index.html"
+  },
+  {
+    "revision": "d90f32ad384f9a4320fa",
+    "url": "/redux-todo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3de1d3d63c14d36347f6",
+    "url": "/redux-todo/static/js/2.3e194455.chunk.js"
+  },
+  {
+    "revision": "d90f32ad384f9a4320fa",
+    "url": "/redux-todo/static/js/main.ba29c9d9.chunk.js"
+  },
+  {
+    "revision": "aaaefad5732d6a6b755e",
+    "url": "/redux-todo/static/js/runtime~main.0bccfe5f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/redux-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
